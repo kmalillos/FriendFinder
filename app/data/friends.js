54e608442669cb75c05bab660b183748c390be85
kmalillos <malillosk@gmail.com>
2019-01-03 14:23:35 -0700

    added code in routes and data

---
+++ app/data/friends.js
@@ -0,0 +1,152 @@
+// DATA
+// =============================================================================
+var friendsData = [
+    {
+    "name": "Ahmed Smith",
+    "photo": "https://images.unsplash.com/photo-1529494078576-f5620a31e7bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1834&q=80",
+    "scores": [
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
+    ]
+    },
+    {
+    "name": "Jacob Scanlon",
+    "photo": "https://images.unsplash.com/photo-1520013633827-6724d7d57eb1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+    ]
+    },
+    {
+    "name": "Anna Jeremiah",
+    "photo": "https://images.unsplash.com/photo-1527999319509-bf0360a1f1df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "scores": [
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "5"
+    ]
+    },
+    {
+    "name": "Delia Louis",
+    "photo": "https://images.unsplash.com/photo-1476535882942-632ab96c48aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80",
+    "scores": [
+    "3",
+    "3",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2",
+    "2",
+    "3"
+    ]
+    },
+    {
+    "name": "Lo Ritter",
+    "photo": "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "Jordan Biason",
+    "photo": "https://images.unsplash.com/photo-1468852677467-06a0f71c46f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=678&q=80",
+    "scores": [
+    "4",
+    "4",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "2",
+    "4",
+    "5"
+    ]
+    },
+    {
+    "name": "Cherry Johnson",
+    "photo": "https://images.unsplash.com/photo-1518566338220-afbbf0cc5093?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1004&q=80",
+    "scores": [
+    "1",
+    "4",
+    "4",
+    "2",
+    "1",
+    "2",
+    "2",
+    "4",
+    "4",
+    "3"
+    ]
+    },
+    {
+    "name": "Patrick Starr",
+    "photo": "https://images.unsplash.com/photo-1472711649025-510d7c09a6fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1059&q=80",
+    "scores": [
+    "2",
+    "4",
+    "4",
+    "2",
+    "3",
+    "4",
+    "2",
+    "5",
+    "5",
+    "2"
+    ]
+    },
+    {
+    "name": "Eileen Mann",
+    "photo": "https://images.unsplash.com/photo-1437711102607-4e4a123a50d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    "scores": [
+    "4",
+    "2",
+    "3",
+    "2",
+    "4",
+    "2",
+    "4",
+    "2",
+    "4",
+    "2"
+    ]
+    }
+    ]
+
+// EXPORT ARRAY
+// =============================================================================
+module.exports = friendsData;
