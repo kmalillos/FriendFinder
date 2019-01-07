dd8d255f5fd970baabd987d512455c8f5fa72d0c
kmalillos <malillosk@gmail.com>
2019-01-06 20:15:15 -0700

    added app.post on apiRoutes and code survey.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,53 @@
 // LOAD DATA
 // =============================================================================
-var friendsData = require("../data/friends");
+var friends = require("../data/friends");
 
 // ROUTING
 // =============================================================================
 module.exports = function(app) {
 
     app.get("/api/friends", function (req, res) {
-        res.json(friendsData);
+        res.json(friends);
     });
 
     app.post("api/friends", function (req, res) {
-        friendsData.push(req.body);
-    })
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            answersDiff: 1000
+        }
+
+        console.log(req.body);
+
+        // holds user input
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        console.log(userScores);
+
+        var totalDiff = 0;
+
+        for (var i=0; i < friends.length; i++) {
+
+            console.log(friends[i]);
+
+            totalDiff = 0;
+
+            for (var j=0; j < friends[i].scores[j]; j++) {
+                totalDiff += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+                if (totalDiff <= bestMatch.answersDiff) {
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.answersDiff = totalDiff;
+                }
+            }
+        }
+
+        friends.push(userData);
+
+        res.json(bestMatch);
+
+    });
 };
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 // DEPENDENCIES
 // =============================================================================
-var path = require("path");
 var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
 
 
 // ROUTING
